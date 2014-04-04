8aed3af680030676c01f2bb164506ba0546547b0
Matthieu Durindel <matthieu.durindel@y-nov.com>
2014-04-04 10:19:10 +0200

    create database

---
+++ app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $hackdumb = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'hackdumb',
+		'database' => 'cake',
+	);
+}
